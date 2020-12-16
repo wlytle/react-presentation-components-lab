d84654b03f9d26a7f27e80d35491ef30d69df321
wlytle <wlytle89@gmail.com>
2020-12-16 11:54:47 -0700

    commit tree 94059acd60c1af38479ff17a4c9f725e46217a1f

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,12 @@
 // Code SimplerComponent Here
+import React from "react";
+
+const SimplerComponent = ({ handleClick, color }) => {
+  return (
+    <div style={{ backgroundColor: color || "white" }} onClick={handleClick}>
+      I am just happy
+    </div>
+  );
+};
+
+export default SimplerComponent;
